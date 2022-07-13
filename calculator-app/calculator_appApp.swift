f0b693736f0c604bfde1c5b3633d84cd2e0dd22d
MaybeJaybe <69604238+MaybeJaybe@users.noreply.github.com>
2022-07-13 00:45:50 -0700

    Initial Commit

---
+++ calculator-app/calculator_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculator_appApp.swift
+//  calculator-app
+//
+//  Created by Jay Mayberry on 7/13/22.
+//
+
+import SwiftUI
+
+@main
+struct calculator_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
